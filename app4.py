40f8cb6042e3903e1da465530aaeca215502b58a
Romulo Cabral <romulo.cabral@grupo.eneva.com.br>
2024-06-21 10:01:12 -0300

    versao-final

--- app4.py
+++ app4.py
@@ -2,28 +2,51 @@ import streamlit as st
 from PIL import Image
 import pytesseract
 from googletrans import Translator
-import os
+import os, cv2
+import numpy as np
+
+os.chdir(os.getcwd())
 
 def define_local_tesseract():
     # Define o caminho do Tesseract no Windows
     try:
-        print(os.getcwd())
-        pytesseract.pytesseract.tesseract_cmd = f'C:\\Users\\{os.getlogin()}\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
+        pytesseract.pytesseract.tesseract_cmd = f'OCR\\tesseract.exe'
+        # pytesseract.pytesseract.tesseract_cmd = f'C:\\Users\\{os.getlogin()}\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
     except:
         print("Caminho do Tesseract não encontrado")
 
 def extrair_texto_da_imagem(caminho_imagem):
     # Carrega a imagem
     imagem = Image.open(caminho_imagem)
+
+    imagem_cv = np.array(imagem)
+    imagem_cv = cv2.cvtColor(imagem_cv, cv2.COLOR_RGB2BGR)
+    
     # Extrai o texto da imagem
     texto = pytesseract.image_to_string(imagem)
-    return texto
+    data = pytesseract.image_to_data(imagem, output_type='dict')
+
+    # Desenhar retângulos ao redor do texto detectado
+    boxes = len(data['level'])
+    for i in range(boxes):
+        (x, y, w, h, text) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i], data['text'][i])
+        if text.strip():  # Verifica se o texto não está vazio
+            # sub_img = imagem_cv[y:y+h, x:x+w]
+            # blur = cv2.GaussianBlur(sub_img, (15, 15), 0)
+            # imagem_cv[y:y+h, x:x+w] = blur
+            imagem_cv = cv2.rectangle(imagem_cv, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    # Convertendo de volta para o formato PIL
+    imagem_rec = Image.fromarray(cv2.cvtColor(imagem_cv, cv2.COLOR_BGR2RGB))
+    
+    return texto, imagem_rec
+
 
 def obter_nome_idioma(idiomas, codigo_idioma):
     return idiomas.get(codigo_idioma, 'Idioma desconhecido') 
 
-define_local_tesseract()
 tradutor = Translator()
+define_local_tesseract()
 # Configuração da página
 st.title('Tradutor de Imagem')
 
@@ -56,22 +79,23 @@ idiomas_nomes = list(idiomas_dict.values())
 idioma_selecionado = st.sidebar.selectbox('Selecione o idioma de destino', idiomas_nomes, index=idiomas_nomes.index('Português'))
 codigo_idioma_destino = list(idiomas_dict.keys())[list(idiomas_dict.values()).index(idioma_selecionado)]
 
-
 if pagina == 'Carregar Imagens':
     # Upload das imagens
     imagens = st.file_uploader('Carregar Imagens', type=['jpg', 'png'], accept_multiple_files=True)
 
     # Se uma ou mais imagens forem carregadas
     if imagens is not None:
-        for i, imagem in enumerate(imagens):
+        for i, caminho_imagem in enumerate(imagens):
             # Exibir a imagem
-            st.image(imagem, caption=f'Imagem Carregada {i+1}', use_column_width=True)
+            st.image(caminho_imagem, caption=f'Imagem Carregada {i+1}', use_column_width=True)
             # Extrair texto da imagem
-            texto_extraido = extrair_texto_da_imagem(imagem)
+            texto_extraido, imagem_rec = extrair_texto_da_imagem(caminho_imagem)
+            st.image(imagem_rec, caption=f'Texto demarcado {i+1}', use_column_width=True)
             # st.write('Texto Extraído:', texto_extraido)
             try:
                 # Traduzir texto
                 traducao = tradutor.translate(texto_extraido, codigo_idioma_destino)
+                # print(traducao.text)
                 st.write('Idioma Detectado:', idiomas_dict[traducao.src])
                 st.write('Texto Traduzido:\n', traducao.text)
             except:
@@ -82,11 +106,13 @@ elif pagina == 'Tirar Foto':
 
     if img_file_buffers is not None:
         # Extrair texto da imagem
-        texto_extraido = extrair_texto_da_imagem(img_file_buffers)
+        texto_extraido, imagem_rec = extrair_texto_da_imagem(img_file_buffers)
+        st.image(imagem_rec, caption=f'Texto demarcado', use_column_width=True)
         # st.write('Texto Extraído:', texto_extraido)
         try:
             # Traduzir texto
             traducao = tradutor.translate(texto_extraido, codigo_idioma_destino)
+            # print(traducao.text)
             st.write('Idioma Detectado:', idiomas_dict[traducao.src])
             st.write('Texto Traduzido:\n', traducao.text)
         except:
@@ -101,7 +127,7 @@ Rômulo Rizo Cabral
 
 #### Sobre o Projeto:
 
-O Tradutor de Imagem é fruto da aplicação prática das técnicas aprendidas em sala de aula, representando um marco na finalização da disciplina de Instalação de Ambiente Computacional, conduzida pelo Professor Thiago Medeiros, na Universidade do Estado do Rio de Janeiro (UERJ). Este projeto não apenas demonstra a habilidade dos estudantes em assimilar e aplicar conhecimentos, mas também marca a criação de um micro serviço em nuvem como resultado final.
+O Tradutor de Imagem é um software de aplicação prática das técnicas aprendidas em sala de aula, representando o trabalho final da disciplina de Instalação de Ambiente Computacional, conduzida pelo Professor Thiago Medeiros, na Universidade do Estado do Rio de Janeiro (UERJ).
 
 #### Objetivo:
 
@@ -113,7 +139,7 @@ O Tradutor de Imagem utiliza técnicas avançadas de processamento de imagem e r
 
 #### Como Funciona:
 
-O Tradutor de Imagem oferece uma interface simples e intuitiva, permitindo aos usuários carregar imagens contendo texto ou capturar novas fotos através da câmera de seus dispositivos. Após o processamento, o texto é automaticamente traduzido para o idioma selecionado, proporcionando uma experiência de tradução eficiente e acessível.
+O Tradutor de Imagem oferece uma interface simples e intuitiva, permitindo aos usuários carregar imagens contendo texto ou capturar novas fotos através da câmera de seus dispositivos. 
 
 ##### Exemplo:
 
@@ -121,13 +147,21 @@ O Tradutor de Imagem oferece uma interface simples e intuitiva, permitindo aos u
 """)
 
     st.image('texto2.jpg', caption=f'Imagem Carregada')
-    texto_extraido = extrair_texto_da_imagem('texto2.jpg')
-
+    texto_extraido, imagem_rec = extrair_texto_da_imagem('texto2.jpg')
+    st.write( """
+- Após o processamento, o texto é identificado e demarcado na imagem, como mostrado na figura abaixo. 
+""" )
+    st.image(imagem_rec, caption=f'Texto demarcado', use_column_width=True)
+    # st.write('Texto Extraído:', texto_extraido)
+    st.write( """
+- Em seguida printada a saída com o idioma identificado no texto e sua tradução para o idioma selecionado, proporcionando uma experiência de tradução eficiente e acessível.
+""" )
     st.write("##### Saída:")
 
     try:
         # Traduzir texto
         traducao = tradutor.translate(texto_extraido, codigo_idioma_destino)
+        # print(traducao.text)
         st.write('Idioma Detectado:', idiomas_dict[traducao.src])
         st.write('Texto Traduzido:\n', traducao.text)
     except:
